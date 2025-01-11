52ff03f1fdcd8b72b9307ded550ce94ab7d59fe8
Gabriel Díaz <gdiaz@qswarm.com>
2025-01-12 00:36:37 +0100

    add services for linux using systemctl

--- lib_sysinfo/linux/services.ml
+++ lib_sysinfo/linux/services.ml
@@ -1,3 +1,48 @@
-type t = string [@@deriving yojson]
+type t =
+  { name : string
+  ; status : string
+  ; enabled : string
+  ; description : string
+  }
+[@@deriving yojson]
 
-let get () = [ Ok "service_a"; Error "service_b" ]
+type systemd_unit =
+  { unit : string
+  ; load : string
+  ; active : string
+  ; sub : string
+  ; description : string
+  }
+[@@deriving yojson]
+
+type systemd_unit_list = systemd_unit list
+[@@deriving yojson]
+
+let get_systemd_services () =
+  let cmd = {|systemctl list-units --output=json|} in
+  let ic = Unix.open_process_in cmd in
+  let rec loop acc =
+    match input_line ic with
+    | exception End_of_file -> acc
+    | line ->
+      let su = systemd_unit_list_of_yojson (Yojson.Safe.from_string line) in
+      (match su with
+       | Ok su ->
+         List.map (fun s ->
+         Ok
+           { name = s.unit
+           ; status = s.sub
+           ; enabled = s.active
+           ; description = s.description
+           }) su
+         @ acc
+       | Error msg ->
+         let _ = Printf.printf "Error: %s\n" msg in
+         loop (Error msg :: acc))
+  in
+  let result = loop [] in
+  let _ = Unix.close_process_in ic in
+  result
+;;
+
+let get () = get_systemd_services ()
--- lib_sysinfo/services.mli
+++ lib_sysinfo/services.mli
@@ -1,4 +1,9 @@
-type t = string
+type t =
+  { name : string
+  ; status : string
+  ; enabled : string
+  ; description : string
+  }
 
 val of_yojson : Yojson.Safe.t -> (t, string) result
 val to_yojson : t -> Yojson.Safe.t
