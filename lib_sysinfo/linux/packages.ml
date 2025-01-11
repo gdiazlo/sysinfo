63c6c609bdfcc2b982c298fc4c4940ecac1afe7f
Gabriel Díaz <gdiaz@qswarm.com>
2025-01-12 00:11:40 +0100

    add rpm packages using rpm command

--- lib_sysinfo/linux/packages.ml
+++ lib_sysinfo/linux/packages.ml
@@ -1,33 +1,67 @@
-type t = {
-  vendor: string;
-  name : string;
-  version: string;
-  format: string;
-  arch: string;
-  description: string;
-  location: string;
-} [@@deriving yojson]
+type t =
+  { vendor : string
+  ; name : string
+  ; version : string
+  ; format : string
+  ; arch : string
+  ; description : string
+  ; location : string
+  }
+[@@deriving yojson]
 
-
+let is_executable file =
+  try
+    let mode = Unix.stat file in
+    mode.Unix.st_perm land 0o100 <> 0 (* Check if the execute permission is set *)
+  with
+  | Unix.Unix_error (_, _, _) ->
+    false (* Return false if an error occurs (e.g., file does not exist) *)
+;;
 
 let get_dpkg_packages () =
-  let cmd = {|dpkg-query -W -f='{"vendor": "${Maintainer}", "name": "${Package}", "version": "${Version}", "arch": "${Architecture}", "location": "dpkg-query", "format": "deb", "description": ""}\n' |} in
+  let cmd =
+    {|dpkg-query -W -f='{"vendor": "${Maintainer}", "name": "${Package}", "version": "${Version}", "arch": "${Architecture}", "location": "dpkg-query", "format": "deb", "description": ""}\n' |}
+  in
   let ic = Unix.open_process_in cmd in
   let rec loop acc =
     match input_line ic with
     | exception End_of_file -> acc
     | line ->
-      let _ = Printf.printf "Line: %s\n" line in
       let pkg = of_yojson (Yojson.Safe.from_string line) in
-      match pkg with
-      | Ok pkg -> loop (Ok pkg :: acc)
-      | Error msg -> Printf.printf "Error: %s\n" msg;
-        loop ( (Error msg) :: acc)
-
+      (match pkg with
+       | Ok pkg -> loop (Ok pkg :: acc)
+       | Error msg ->
+         loop (Error msg :: acc))
   in
   let result = loop [] in
   let _ = Unix.close_process_in ic in
   result
+;;
+
+let get_rpm_packages () =
+  let cmd =
+    {|rpm -qa --qf='\{"vendor": "%{VENDOR}", "name": "%{NAME}", "version": "%{VERSION}", "arch": "%{ARCH}", "location": "rpm", "format": "rpm", "description": "%{SUMMARY}"\}\n'|}
+  in
+  let ic = Unix.open_process_in cmd in
+  let rec loop acc =
+    match input_line ic with
+    | exception End_of_file -> acc
+    | line ->
+      let pkg = of_yojson (Yojson.Safe.from_string line) in
+      (match pkg with
+       | Ok pkg -> loop (Ok pkg :: acc)
+       | Error msg ->
+         loop (Error msg :: acc))
+  in
+  let result = loop [] in
+  let _ = Unix.close_process_in ic in
+  result
+;;
 
 let get () =
-  get_dpkg_packages ()
+  if is_executable "/usr/bin/dpkg-query"
+  then get_dpkg_packages ()
+  else if is_executable "/usr/bin/rpm"
+  then get_rpm_packages ()
+  else [ Error "No package manager found" ]
+;;
--- lib_sysinfo/packages.mli
+++ lib_sysinfo/packages.mli
@@ -1,12 +1,12 @@
-type t = {
-  vendor: string;
-  name : string;
-  version: string;
-  format: string;
-  arch: string;
-  description: string;
-  location: string;
-}
+type t =
+  { vendor : string
+  ; name : string
+  ; version : string
+  ; format : string
+  ; arch : string
+  ; description : string
+  ; location : string
+  }
 
 val of_yojson : Yojson.Safe.t -> (t, string) result
 val to_yojson : t -> Yojson.Safe.t
